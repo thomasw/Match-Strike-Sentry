38a8538e33dc15bdab2f70031e75eb5f2ad5f739
thomasw <thomas@matchstrike.net>
2010-11-26 18:15:26 -0500

    Added sentry to the root urls file.

--- urls.py
+++ urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
 urlpatterns = patterns('',
-	# Example:
-	# (r'^project_template/', include('project_template.foo.urls')),
-	
+	(r'', include('sentry.urls')),
 	(r'^assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root':getattr(settings,'MEDIA_ROOT')}),
 	url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout')
 )
